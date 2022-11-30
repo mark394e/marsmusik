e9228ce9496e9249d89025e59dee305b3eb1de47
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-11-30 13:35:17 +0100

    schedule og bands er merged

---
+++ pages/test.js
@@ -0,0 +1,23 @@
+[
+  {
+    name: "A Perfect Circle",
+    members: [
+      "Maynard James Keenan",
+      "Billy Howerdel",
+      "James Iha",
+      "Matt McJunkins",
+      "Jeff Friedl",
+    ],
+    genre: "Alternative Rock",
+    logoCredits:
+      "By Michael John Stinsman - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=15811084",
+    logo: "A_Perfect_Circle_Logo_2011_-_Michael_John_Stinsman_InvisibleStudio_Productions.png",
+    bio: "A Perfect Circle is an American rock supergroup formed in 1999 by guitarist Billy Howerdel and Tool vocalist Maynard James Keenan. A Perfect Circle has released four studio albums, the first three during the early 2000s: Mer de Noms, their debut album in 2000, and followed up by Thirteenth Step in 2003; then in 2004, Emotive—an album of radically re-worked cover songs. Shortly after Emotive's release, the band went on hiatus; Keenan returned to Tool and started up solo work under the band name Puscifer. and Howerdel released a solo album, Keep Telling Myself It's Alright, under the moniker Ashes Divide. Band activity was sporadic in the following years; the band reformed in 2010, and played live shows on and off between 2010 and 2013, but fell into inactivity after the release of their greatest hits album, Three Sixty, and a live album box set, A Perfect Circle Live: Featuring Stone and Echo in late 2013. The band reformed in 2017 to record a fourth album, Eat the Elephant, which was released on April 20, 2018.",
+    start: "00:00",
+    end: "02:00",
+    act: "Greenfelder, Haag and Lakin",
+    cancelled: true,
+    day: "mon",
+    stage: "Midgard",
+  },
+];
