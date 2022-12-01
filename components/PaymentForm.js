0c93a9572da29bd9fa466cf6833ce76d3f37e3c6
Markus <mark394e@stud.kea.dk>
2022-12-01 10:30:19 +0100

    rettet til korrekte react attributter i forms

--- components/PaymentForm.js
+++ components/PaymentForm.js
@@ -5,55 +5,55 @@ function PaymentForm() {
     <>
       <section id="paymentform">
         <form className="forms">
-          <label for="fullname">
+          <label htmlFor="fullname">
             {" "}
             Full name
             <input type="text" id="fullname" name="fullname" />
           </label>
-          <label for="email">
+          <label htmlFor="email">
             E-mail
             <input type="email" id="email" name="email" />
           </label>
-          <label for="address">
+          <label htmlFor="address">
             Address
             <input type="text" id="address" name="address" />
           </label>
-          <label for="zipcode">
+          <label htmlFor="zipcode">
             Zip code
-            <input type="text" inputmode="numeric" id="zipcode" name="zipcode" />
+            <input type="text" inputMode="numeric" id="zipcode" name="zipcode" />
           </label>
-          <label for="city">
+          <label htmlFor="city">
             City
             <input type="text" id="city" name="city" />
           </label>
         </form>
 
         <form className="forms">
-          <label for="cardholder">
+          <label htmlFor="cardholder">
             Cardholder name
             <input type="text" id="cardholder" name="cardholder" />
           </label>
-          <label for="cardnumber">
+          <label htmlFor="cardnumber">
             Credit card number
             <input
               type="text"
-              inputmode="numeric"
-              maxlength="16"
+              inputMode="numeric"
+              maxLength="16"
               id="cardnumber"
               name="cardnumber"
             />
           </label>
-          <label for="month">
+          <label htmlFor="month">
             Month
-            <input type="text" inputmode="numeric" maxlength="2" id="month" name="month" />
+            <input type="text" inputMode="numeric" maxLength="2" id="month" name="month" />
           </label>
-          <label for="year">
+          <label htmlFor="year">
             Year
-            <input type="text" inputmode="numeric" maxlength="2" id="year" name="year" />
+            <input type="text" inputMode="numeric" maxLength="2" id="year" name="year" />
           </label>
-          <label for="cvv">
+          <label htmlFor="cvv">
             CVV
-            <input type="text" inputmode="numeric" maxlength="3" id="cvv" name="cvv" />
+            <input type="text" inputMode="numeric" maxLength="3" id="cvv" name="cvv" />
           </label>
         </form>
       </section>
--- components/TicketHolder.js
+++ components/TicketHolder.js
@@ -4,12 +4,12 @@ function TicketHolder() {
   return (
     <>
       <form id="ticketholderform">
-        <label for="fullname">
+        <label htmlFor="fullname">
           {" "}
           Full name
           <input type="text" id="fullname" name="fullname" />
         </label>
-        <label for="email">
+        <label htmlFor="email">
           E-mail
           <input type="email" id="email" name="email" />
         </label>
