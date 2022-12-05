e237ee5a1b5af826f753bde7189c2f0f54e03e37
Marikka Katrina Højen Jørgensen <mari46re@stud.kea.dk>
2022-12-05 09:09:53 +0100

    nav in progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.scss";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
