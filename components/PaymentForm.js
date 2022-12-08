c3245c0c542ba170810ee9ec369ee8e580409319
janne hjuler jensen <jann778j@stud.kea.dk>
2022-12-08 14:40:45 +0100

    payment fomr some way - not finished

--- components/PaymentForm.js
+++ components/PaymentForm.js
@@ -1,6 +1,7 @@
 import "../styles/PaymentForm.module.scss";
 import { insertOrder } from "../modules/db";
 import { useRef } from "react";
+import { useState } from "react";
 
 function PaymentForm(props) {
   const theForm = useRef(null);
@@ -19,14 +20,40 @@ function PaymentForm(props) {
   }
   // det her virker hvis man fjerner labels... så vil den hoppe fra input felt til inputfelt
   // når maxlenght er her nået - meeeeeen ligenu er lorte labels ivejen mææhh
-  document.querySelectorAll("input").forEach((el) => {
-    el.addEventListener("input", (e) => {
-      if (e.target.value.length == e.target.maxLength) {
-        console.log("something ");
-        e.target.parentElement.nextElementSibling.firstElementChild.focus();
-      }
-    });
-  });
+  // document.querySelectorAll("input").forEach((el) => {
+  //   el.addEventListener("input", (e) => {
+  //     if (e.target.value.length == e.target.maxLength) {
+  //       console.log("something ");
+  //       e.target.parentElement.nextElementSibling.firstElementChild.focus();
+  //     }
+  //   });
+  // });
+
+  function inputChange(e) {
+    if (e.target.value.length == e.target.maxLength) {
+      console.log("something ");
+      e.target.parentElement.nextElementSibling.firstElementChild.focus();
+    }
+  }
+
+  // KODE FUNDET HER !! https://bobbyhadz.com/blog/react-check-if-email-is-valid
+
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const handleChange = (event) => {
+    if (!isValidEmail(event.target.value)) {
+      setError("Email is invalid");
+    } else {
+      setError(null);
+    }
+
+    setEmail(event.target.value);
+  };
 
   return (
     <>
@@ -35,28 +62,60 @@ function PaymentForm(props) {
           <label htmlFor="fullname">
             {" "}
             Full name
-            <input type="text" id="fullname" name="fullname" />
+            <input
+              type="text"
+              id="fullname"
+              name="fullname"
+              required
+              placeholder="Full name "
+            />
           </label>
           <label htmlFor="email">
             E-mail
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="abc@gmail.com"
+              // required
+              value={email}
+              onChange={handleChange}
+            />
+            {error && <p style={{ color: "red" }}>{error}</p>}
           </label>
+
           <label htmlFor="address">
             Address
-            <input type="text" id="address" name="address" />
+            <input
+              type="text"
+              id="address"
+              name="address"
+              required
+              placeholder="Address"
+            />
           </label>
           <label htmlFor="zipcode">
             Zip code
             <input
+              onInput={inputChange}
               type="text"
               inputMode="numeric"
               id="zipcode"
               name="zipcode"
+              maxLength={4}
+              required
+              placeholder="Zip code"
             />
           </label>
           <label htmlFor="city">
             City
-            <input type="text" id="city" name="city" />
+            <input
+              type="text"
+              id="city"
+              name="city"
+              required
+              placeholder="City"
+            />
           </label>
           <button>submit</button>
         </form>
@@ -64,36 +123,51 @@ function PaymentForm(props) {
         <form className="forms">
           <label htmlFor="cardholder">
             Cardholder name
-            <input type="text" id="cardholder" name="cardholder" />
+            <input
+              type="text"
+              id="cardholder"
+              name="cardholder"
+              required
+              placeholder="Cardholder name"
+            />
           </label>
           <label htmlFor="cardnumber">
             Credit card number
             <input
+              onInput={inputChange}
               type="text"
               inputMode="numeric"
               maxLength={16}
               id="cardnumber"
               name="cardnumber"
+              required
+              placeholder="XXXXXXXXXXXXXXXX"
             />
           </label>
           <label htmlFor="month">
             Month
             <input
+              onInput={inputChange}
               type="text"
               inputMode="numeric"
               maxLength={2}
               id="month"
               name="month"
+              placeholder="XX"
+              required
             />
           </label>
           <label htmlFor="year">
             Year
             <input
+              onInput={inputChange}
               type="text"
               inputMode="numeric"
               maxLength={2}
               id="year"
               name="year"
+              placeholder="XX"
+              required
             />
           </label>
           <label htmlFor="cvv">
@@ -104,6 +178,8 @@ function PaymentForm(props) {
               maxLength={3}
               id="cvv"
               name="cvv"
+              placeholder="XXX"
+              required
             />
           </label>
         </form>
