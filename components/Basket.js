5b7479c3f41988351ba8530036bdc36c226220b0
Markus <mark394e@stud.kea.dk>
2022-12-16 17:03:47 +0100

    tickets mobile styling

--- components/Basket.js
+++ components/Basket.js
@@ -63,72 +63,74 @@ function Basket(props) {
   return (
     <section className={styles.basket}>
       <h3>Your order</h3>
-      <div>
-        <h4>TICKETS</h4>
-        <ul>
-          {showVIPTicket && (
-            <li>
-              {/* her henter vi vores contervip ind som er sat til at tælle hvor mange 
-          biletter der er valgt og gange det valgte billettal med pricevip som er sat til en bestemt pris 
-          for så at få en total værdi af de valgte biletter lagt sammen  */}
-              {props.counterVIP} &#10799; VIP {props.counterVIP * prices.priceVIP},-
-            </li>
-          )}
-          {showREGTicket && (
-            <li>
-              {props.counterREG} &#10799; Standard {props.counterREG * prices.priceREG}
-              ,-
-            </li>
-          )}
-        </ul>
-      </div>
-      <div>
-        <h4>CAMPING</h4>
-        <p>
-          {!props.showPickedCamping ? (
-            "None"
-          ) : (
-            <>
-              {props.pickedCamping} <span> 99,-</span>
-            </>
-          )}
-        </p>
-      </div>
-      <div>
-        <h4>EXTRAS</h4>
-        <ul>
-          {showGreenCamp && (
-            <li>
-              {props.counterGreenCamp}x Green Camping{" "}
-              {props.counterGreenCamp * prices.priceGreenCamp},-
-            </li>
-          )}
-          {showPrebuildTwo && (
-            <li>
-              {props.counterPrebuildTwo}x 2 pers. tent{" "}
-              {props.counterPrebuildTwo * prices.pricePrebuildTwo},-
-            </li>
-          )}
-          {showPrebuildThree && (
-            <li>
-              {props.counterPrebuildThree}x 3 pers. tent{" "}
-              {props.counterPrebuildThree * prices.pricePrebuildThree},-
-            </li>
-          )}
-        </ul>
-      </div>
-      <hr />
-      <h3>Total {priceTotal + priceCampingspot},-</h3>
       {props.showTimer && (
         <Countdown
           date={props.timer}
           renderer={(props) => (
-            <div>
+            <div className="basketTimer">
               {props.minutes}:{props.seconds}
             </div>
           )}
         ></Countdown>
       )}
+      <div className="mobileBasket">
+        <div>
+          <h4>TICKETS</h4>
+          <ul>
+            {showVIPTicket && (
+              <li>
+                {/* her henter vi vores contervip ind som er sat til at tælle hvor mange 
+          biletter der er valgt og gange det valgte billettal med pricevip som er sat til en bestemt pris 
+          for så at få en total værdi af de valgte biletter lagt sammen  */}
+                {props.counterVIP} &#10799; VIP {props.counterVIP * prices.priceVIP},-
+              </li>
+            )}
+            {showREGTicket && (
+              <li>
+                {props.counterREG} &#10799; Standard {props.counterREG * prices.priceREG}
+                ,-
+              </li>
+            )}
+          </ul>
+        </div>
+        <div>
+          <h4>CAMPING</h4>
+          <p>
+            {!props.showPickedCamping ? (
+              "None"
+            ) : (
+              <>
+                {props.pickedCamping} <span> 99,-</span>
+              </>
+            )}
+          </p>
+        </div>
+        <div>
+          <h4>EXTRAS</h4>
+          <ul>
+            {showGreenCamp && (
+              <li>
+                {props.counterGreenCamp}x Green Camping{" "}
+                {props.counterGreenCamp * prices.priceGreenCamp},-
+              </li>
+            )}
+            {showPrebuildTwo && (
+              <li>
+                {props.counterPrebuildTwo}x 2 pers. tent{" "}
+                {props.counterPrebuildTwo * prices.pricePrebuildTwo},-
+              </li>
+            )}
+            {showPrebuildThree && (
+              <li>
+                {props.counterPrebuildThree}x 3 pers. tent{" "}
+                {props.counterPrebuildThree * prices.pricePrebuildThree},-
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <h3>Total {priceTotal + priceCampingspot},-</h3>
     </section>
   );
 }
